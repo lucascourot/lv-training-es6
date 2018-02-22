bf1d454685c613d14d4db5c1154922fac5973883
Lucas Courot <lucas@courot.com>
2018-02-22 17:05:57 +0100

    Fix redux

--- front/src/store.js
+++ front/src/store.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import mainReducer from './reducers/index'
 
-export default createStore(combineReducers(mainReducer))
+export default createStore(
+    combineReducers(mainReducer),
+    {},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
